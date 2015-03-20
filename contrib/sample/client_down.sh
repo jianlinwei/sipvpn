5ee8a1e10938f11d78ab6799913fe66d2d30cf1f
Xiaoxiao Pu <i@xiaoxiao.im>
2015-03-21 02:26:54 +0800

    update hook scripts

--- contrib/sample/client_down.sh
+++ contrib/sample/client_down.sh
@@ -11,7 +11,7 @@
 
 # delete route
 if ! [[ $(ip route get $server | grep local) ]]; then
-	ip route del $(ip route get $server | grep via)
+	ip route del $(ip route get $server | sed -n 1p)
 fi
 ip route del 0.0.0.0/1 via 10.12.5.2 dev $tunif
 ip route del 128.0.0.0/1 via 10.12.5.2 dev $tunif
--- test/client_up.sh
+++ test/client_up.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-# sample client up script
+# available environment variables:
+#   $mode
+#   $server
+#   $port
+#   $tunif
+#   $mtu
+#   $up
+#   $down
 
+# configure tun interface
 ip link set $tunif up
 ip link set $tunif mtu $mtu
 ip addr add 10.12.5.2/24 dev $tunif
 
+# add route
 if ! [[ $(ip route get $server | grep local) ]]; then
 	ip route replace $(ip route get $server | sed -n 1p)
 fi
